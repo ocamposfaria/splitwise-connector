4668cd5b212ff5eb8254b27cc06f458bfeb8c1a2
João Pedro Campos Faria <ocamposfaria@gmail.com>
2025-05-05 19:23:39 -0300

    __init__

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,4 @@
 from icecream import ic
 
 ic('Acesse o Streamlit em --> http://127.0.0.1:8501/')
-ic('Acesse o Fast API em ---> http://127.0.0.1:8000/')
+ic('Acesse o Fast API em ---> http://127.0.0.1:8000/docs#/')
